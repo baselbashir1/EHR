78db361021f1ebfadc9e176046a82db2b5dfa8ed
Mhd Basel Bashir <u13472@isoint.net>
2024-07-04 15:54:04 +0300

    fix login

--- user-service/src/main/java/com/medical/ehr/services/AuthService.java
+++ user-service/src/main/java/com/medical/ehr/services/AuthService.java
@@ -9,22 +9,29 @@ import com.medical.ehr.models.User;
 import com.medical.ehr.repositories.UserRepository;
 import com.medical.ehr.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthService {
 
+    // replace this with service
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
-    private final AuthenticationManager authenticationManager;
 
     public RegisterResponse register(RegisterRequest registerRequest) {
 
@@ -55,14 +62,17 @@ public class AuthService {
     }
 
     public LoginResponse login(LoginRequest loginRequest) {
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password());
-        authenticationManager.authenticate(usernamePasswordAuthenticationToken);
-
-        User user = userRepository.findByUsername(loginRequest.username())
-                .orElseThrow(() -> new UsernameNotFoundException("Username or password incorrect."));
-
-        String token = jwtUtil.generateToken(user);
-        return new LoginResponse(token);
+        Optional<User> user = userRepository.findByUsername(loginRequest.username());
+        if (user.isPresent()) {
+            if (passwordEncoder.matches(loginRequest.password(), user.get().getPassword())) {
+                String token = jwtUtil.generateToken(user.get());
+                return new LoginResponse(token);
+            } else {
+                throw new IllegalArgumentException("Username or password incorrect.");
+            }
+        } else {
+            throw new IllegalArgumentException("Username or password incorrect.");
+        }
     }
 
 }
--- user-service/src/main/java/com/medical/ehr/services/UserService.java
+++ user-service/src/main/java/com/medical/ehr/services/UserService.java
@@ -1,32 +1,21 @@
 package com.medical.ehr.services;
 
-import com.medical.ehr.dto.requests.RegisterRequest;
-import com.medical.ehr.models.User;
 import com.medical.ehr.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
-
-    private static final Logger logger = Logger.getLogger(UserService.class.getName());
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public void register(RegisterRequest registerRequest) {
-        User user = User.builder()
-                .firstname(registerRequest.firstname())
-                .lastname(registerRequest.lastname())
-                .username(registerRequest.username())
-                .email(registerRequest.email())
-                .password(passwordEncoder.encode(registerRequest.password()))
-                .build();
-        userRepository.save(user);
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
     }
-
 }
